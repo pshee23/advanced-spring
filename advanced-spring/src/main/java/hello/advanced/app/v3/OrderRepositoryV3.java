3fcbd5fdbcf09bebabbcf1702847d259d348f868
pshee23 <mabimania23@naver.com>
2022-08-20 14:16:26 +0900

    [로그 추적기] 필드 동기화 - 적용

---
+++ advanced-spring/src/main/java/hello/advanced/app/v3/OrderRepositoryV3.java
@@ -0,0 +1,41 @@
+package hello.advanced.app.v3;
+
+import org.springframework.stereotype.Repository;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV3 {
+
+	private final LogTrace trace;
+	
+	public void save(String itemId) {
+		
+		TraceStatus status = null;
+		try {
+			status = trace.begin("OrderRepository.save()");
+			
+			// 저장 로직
+			if (itemId.equals("ex")) {
+				throw new IllegalStateException("예외 발생!");
+			}
+			sleep(1000);
+			
+			trace.end(status);
+		} catch (Exception e) {
+			trace.exception(status, e);
+			throw e; // 예외를 꼭 다시 던져주어야 한다. 요구 사항에 원래 프로그램에 영향을 끼치면 안되기 때문
+		}
+	}
+
+	private void sleep(int millies) {
+		try {
+			Thread.sleep(millies);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
