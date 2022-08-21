eff6025ea4fb5fa626e7eb6a71a545b2b213d3fd
pshee23 <mabimania23@naver.com>
2022-08-21 12:53:23 +0900

    [로그 추적기] 템플릿 메서드 패턴 - 예제 1 & 2

---
+++ advanced-spring/src/test/java/hello/advanced/trace/template/code/SubClassLogic2.java
@@ -0,0 +1,12 @@
+package hello.advanced.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SubClassLogic2 extends AbstractTemplate {
+	// 변하는 부분인 비즈니스 로직2을 처리하는 자식 클래스. 템플릿이 호출하는 대상인 call() 메서드를 오버라이딩한다.
+	@Override
+	protected void call() {
+		log.info("비즈니스 로직2 실행");
+	}
+}
